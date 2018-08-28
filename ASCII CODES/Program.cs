e1bdf97b2c94a9ea471ac0f67db32f925d6b358d
Hafiznaufalr <hafiz.nr5758@gmail.com>
2018-08-28 14:07:07 +0700

    first commit

---
+++ ASCII CODES/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using static System.Console;
+
+namespace ASCII_CODES
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int X = 48; X <= 57; X++)
+            { WriteLine(" ASCII ke {0} ={1} ", X, (char)X); }
+            WriteLine();
+
+            for (int X = 65; X <= 70; X++)
+            { WriteLine(" ASCII ke {0} ={1} ", X, (char)X); }
+            WriteLine();
+
+            for (int X = 97; X <= 102; X++)
+            { WriteLine(" ASCII ke {0} ={1} ", X, (char)X); }
+            WriteLine();
+            ReadKey();
+        }
+    }
+}
